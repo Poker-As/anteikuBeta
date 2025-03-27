e4330161eb099feb0634ab215691ba5762e58173
Poker-As <dvillalba307@gmail.com>
2025-03-27 11:09:03 -0500

    primer commit

---
+++ src/main/java/com/mycompany/cafeteria/Cafeteria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cafeteria;
+
+/**
+ *
+ * @author dvill
+ */
+public class Cafeteria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
